8d424925c38a62992ec660c1d43b4f4461b4dd00
justcoom <vbruner11@mail.ru>
2024-10-19 14:16:50 +0700

    type of data

---
+++ 07-lesson/main.js
@@ -0,0 +1,10 @@
+console.log(5 == '5') // true
+console.log(5 === '5') //false
+console.log('0' == 0) //true
+console.log('0' === 0) //false
+console.log(null == undefined) //true
+console.log('0' == false) //true
+console.log('0' === false) //false
+console.log('true' == true) //false
+ 
+console.log({} == {}) //false
